e43bc1cdae053810153353c50e02f33b054826ad
Benjamin M <schwalegos@gmail.com>
2017-07-21 17:15:13 -0600

    interpolation, camera offset

--- public/js/index.js
+++ public/js/index.js
@@ -439,8 +439,8 @@ var stage = {
 			minx = Math.min(props[i].x, minx);
 			miny = Math.min(props[i].y, miny);
 		}
-		minx -= 100;
-		miny -= 100;
+		minx -= 1000;
+		miny -= 1000;
 		this.bound(minx, miny);
 		cam.offsetx = -minx;
 		cam.offsety = -miny;
@@ -709,7 +709,7 @@ function Actor(type, x, y, w, h, name, sid, gid) {
 				}
 				break;
 			case "friend":
-				this.interp = lerp(this.interp, 1, 0.5);
+				this.interp = lerp(this.interp, 1, 0.25);
 				cubicHermite(
 					[this.x, this.y], [this.vx, this.vy], [this.nx, this.ny], [this.nvx, this.nvy],
 					this.interp, this.npos
